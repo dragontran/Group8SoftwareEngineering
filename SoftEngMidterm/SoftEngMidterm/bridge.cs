50b4d118e6ca3839169c474a612e906318893023
unknown <dragon.tran@ou.edu>
2017-10-27 13:13:15 -0500

    Adds VS project dir

---
+++ SoftEngMidterm/SoftEngMidterm/bridge.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftEngMidterm
+{
+    class Bridge
+    {
+        class Node
+        {
+            public int value;
+            public Node prev, next;
+
+            public Node(int value)
+            {
+                this.value = value;
+            }
+        }
+
+        class StackArray
+        {
+            private int[] items;
+            private int size = -1;
+
+            public StackArray()
+            {
+                this.items = new int[12];
+            }
+
+            public StackArray(int cells)
+            {
+                this.items = new int[cells];
+            }
+
+            public virtual void push(int i)
+            {
+                if (!isFull())
+                {
+                    items[++size] = i;
+                }
+            }
+
+            public bool isEmpty()
+            {
+                return size == -1;
+            }
+
+            public bool isFull()
+            {
+                return size == items.Length - 1;
+            }
+
+            public int top()
+            {
+                if (isEmpty())
+                {
+                    return -1;
+                }
+                return items[size];
+            }
+
+            public virtual int pop()
+            {
+                if (isEmpty())
+                {
+                    return -1;
+                }
+                return items[size--];
+            }
+        }
+
+        class StackList
+        {
+            private Node last;
+
+            public void push(int i)
+            {
+                if (last == null)
+                {
+                    last = new Node(i);
+                }
+                else
+                {
+                    last.next = new Node(i);
+                    last.next.prev = last;
+                    last = last.next;
+                }
+            }
+
+            public bool isEmpty()
+            {
+                return last == null;
+            }
+
+            public bool isFull()
+            {
+                return false;
+            }
+
+            public int top()
+            {
+                if (isEmpty())
+                {
+                    return -1;
+                }
+                return last.value;
+            }
+
+            public int pop()
+            {
+                if (isEmpty())
+                {
+                    return -1;
+                }
+                int ret = last.value;
+                last = last.prev;
+                return ret;
+            }
+        }
+
+        class StackFIFO : StackArray
+        {
+            private StackArray stackArray = new StackArray();
+
+            public override int pop()
+            {
+                while (!isEmpty())
+                {
+                    stackArray.push(base.pop());
+                }
+                int ret = stackArray.pop();
+                while (!stackArray.isEmpty())
+                {
+                    push(stackArray.pop());
+                }
+                return ret;
+            }
+        }
+
+        class StackHanoi : StackArray
+        {
+            private int totalRejected = 0;
+
+            public int reportRejected()
+            {
+                return totalRejected;
+            }
+
+            public override void push(int i)
+            {
+                if (!isEmpty() && i > top())
+                {
+                    totalRejected++;
+                }
+                else
+                {
+                    base.push(i);
+                }
+            }
+        }
+        // class BridgeDisc
+        // {
+        //     static void Main(string[] args)
+        //     {
+        //         StackArray[] stacks = { new StackArray(), new StackFIFO(), new StackHanoi() };
+        //         StackList stackList = new StackList();
+        //         for (int i = 1; i < 15; i++)
+        //         {
+        //             stacks[0].push(i);
+        //             stackList.push(i);
+        //             stacks[1].push(i);
+        //         }
+        //
+        //         Random random = new Random();
+        //         for (int i = 1; i < 15; i++)
+        //         {
+        //             stacks[2].push(random.Next(0, 20));
+        //         }
+        //         while (!stacks[0].isEmpty())
+        //         {
+        //             Console.Write(stacks[0].pop() + " ");
+        //         }
+        //         Console.WriteLine();
+        //         while (!stackList.isEmpty())
+        //         {
+        //             Console.Write(stackList.pop() + " ");
+        //         }
+        //         Console.WriteLine();
+        //         while (!stacks[1].isEmpty())
+        //         {
+        //             Console.Write(stacks[1].pop() + " ");
+        //         }
+        //         Console.WriteLine();
+        //         while (!stacks[2].isEmpty())
+        //         {
+        //             Console.Write(stacks[2].pop() + " ");
+        //         }
+        //         Console.WriteLine();
+        //         Console.WriteLine("total rejected is " + ((StackHanoi)stacks[2]).reportRejected());
+        //     }
+        // }
+    }
+}
---
+++ SoftEngMidterm/SoftEngMidterm/iteratorTest.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoftEngMidterm;
+
+namespace DesignPatterns
+{
+    [TestClass]
+    public class TestIntSet
+    {
+        [TestMethod]
+        public void TestIterationFirstWithOneElement()
+        {
+            IntSet set = new IntSet();
+            set.add(2);
+            
+            IntSet.Iterator it1 = set.createIterator();
+
+            //  Newly created iterator should point to the first number in the list
+            it1.first();
+            Assert.AreEqual(it1.currentValue(), 2);
+        }
+
+        [TestMethod]
+        public void TestIterationFirstWithMoreThanOneElement()
+        {
+            IntSet set = new IntSet();
+            set.add(2);
+            set.add(3);
+            
+            IntSet.Iterator it1 = set.createIterator();
+
+            //  Newly created iterator should point to the first number that was added into the list
+            it1.first();
+            Assert.AreEqual(it1.currentValue(), 2);
+        }
+
+        [TestMethod]
+        public void TestIterationIsDoneWithOneElement()
+        {
+            IntSet set = new IntSet();
+            set.add(2);
+            
+            IntSet.Iterator it1 = set.createIterator();
+
+            it1.first();
+            Assert.AreEqual(it1.currentValue(), 2);
+
+            //  Iterator is still set to the first value so isDone() should return false
+            Assert.AreEqual(it1.isDone(), false);
+
+            //  Try and move to non-existent element, should set currentValue() to -1
+            it1.next();
+            Assert.AreEqual(it1.currentValue(), -1);
+            
+            //  Iterator tried to go to the second element, but it doesn't exist so isDone should now return true
+            Assert.AreEqual(it1.isDone(), true);
+
+        }
+
+        [TestMethod]
+        public void TestIterationNextAndIsDoneWithTwoElement()
+        {
+            IntSet set = new IntSet();
+            set.add(2);
+            set.add(3);
+
+            IntSet.Iterator it1 = set.createIterator();
+
+            it1.first();
+            Assert.AreEqual(it1.currentValue(), 2);
+
+            //  Iterator is still set to the first value so isDone() should return false
+            Assert.AreEqual(it1.isDone(), false);
+
+            //  Move to next element, should set currentValue() to 3
+            it1.next();
+            Assert.AreEqual(it1.currentValue(), 3);
+
+            //  Try and move to non-existent element, should set currentValue() to -1
+            it1.next();
+
+            //  Iterator tried to go to the third element, but it doesn't exist so isDone should now return true
+            Assert.AreEqual(it1.isDone(), true);
+
+        }
+
+        [TestMethod]
+        public void TestIntSetGetListFromSet()
+        {
+            IntSet set = new IntSet();
+            set.add(2);
+
+            List<int> l = set.getList();
+
+            Assert.AreEqual(l, set.getList());
+        }
+
+        [TestMethod]
+        public void TestIntSetIsMember()
+        {
+            IntSet set = new IntSet();
+            set.add(2);
+
+            Assert.AreEqual(set.isMember(2), true);
+            Assert.AreEqual(set.isMember(1), false);
+        }
+    }
+}
