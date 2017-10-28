d0cfe4083e523cbb7dfcb9a89fdc62d983fd5ef6
Unknown <dragon.tran@ou.edu>
2017-10-28 02:32:15 -0500

    Adds a unit test for factory

---
+++ SoftEngMidterm/SoftEngMidterm/factoryTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoftEngMidterm;
+
+
+namespace DesignPatterns
+{
+
+    [TestClass]
+    public class TestFactoryStackArray
+    {
+
+        [TestMethod]
+        public void TestFactoryStackArrayCreation()
+        {
+
+            stackFactory stackArray = new stack();
+
+            Assert.AreEqual((stackArray).getStackObject().GetItemLength(), new int[12].Length);
+            Assert.AreEqual((stackArray).getStackObject().GetSize(), -1);
+        }
+
+        [TestMethod]
+        public void TestGetStackObject()
+        {
+            stackFactory stackArray = new stack();
+
+            Assert.IsInstanceOfType((stackArray).getStackObject(), typeof(StackArray));
+        }
+    }
+}
