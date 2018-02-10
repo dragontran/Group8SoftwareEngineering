42f2d1ae2df4d6fced4ab4b05bdee9c3953dc7dc
andrew <sandshrew@users.noreply.github.com>
2018-02-10 13:56:11 -0600

    Dynamically chooses split size, splitting into 10 parts in most cases

--- caci/Splitter.java
+++ caci/Splitter.java
@@ -5,28 +5,35 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 
 public class Splitter {	
-	private static long PART_SIZE = 1024*100; // Read in 100kb  sections
+
+	final static int MAX_SPLIT = 500000000;
+	final static int MIN_SPLIT = 100;
 	
 	public static void split(String filename){
 		try {
+			Instant start = Instant.now();
 			File inputFile = new File(filename);
+			long splitSize = getSplitSize(inputFile.length());
 			FileInputStream fis;
 			String newFileName;
 			FileOutputStream filePart;
 			int fileSize = (int) inputFile.length();
 			int nChunks = 0;
 			int read = 0; 
-			int readLength = (int)PART_SIZE;
+			int readLength = (int)splitSize;
 			byte[] byteChunkPart;
-			
+
 			System.out.println(filename + " is " + inputFile.length() + " bytes long");
-			
+
+
 			fis = new FileInputStream(inputFile);
 			while (fileSize > 0) {
 				// don't read past the end of the file
-				if (fileSize <= PART_SIZE) {
+				if (fileSize <= splitSize) {
 					readLength = fileSize;
 				}
 				byteChunkPart = new byte[readLength];
@@ -42,12 +49,31 @@ public class Splitter {
 				filePart = null;
 				nChunks++;
 			}
+			Instant end = Instant.now();
 			System.out.println(inputFile.getName() + " was split into " + (nChunks-1) + " parts");
+			Duration diff = Duration.between(start, end);
+			System.out.println("Time elapsed: " + diff.toMillis() + " ms");
+
 			fis.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static long getSplitSize(long nbytes){
+		long splitsize = nbytes;
+		
+		// min 100 byte split
+		if (nbytes > MIN_SPLIT){
+			splitsize = nbytes/10 + nbytes%10;
+			// max 500mb split
+			if (splitsize > MAX_SPLIT){
+				splitsize = MAX_SPLIT;
+			}
+		}
+
+		return splitsize;
 	}
 }
