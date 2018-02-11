1937bc0ccb3373d0fa95528808554d199c74343f
Addison <addison.womack@ou.edu>
2018-02-11 01:04:28 -0600

    Removal of midterm stuff from last semester

--- Splitter/Splitter.java
+++ Splitter/Splitter.java
@@ -1,10 +1,6 @@
 package Splitter;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.time.Duration;
 import java.time.Instant;
 
