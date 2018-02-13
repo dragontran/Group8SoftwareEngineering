d250c3032a7b89532f34fe5500d88e609a304bc2
andrew <sandshrew@users.noreply.github.com>
2018-02-13 02:17:34 -0600

    Compares original checksum with assembled checksum

--- Assembler/Assembler.java
+++ Assembler/Assembler.java
@@ -1,8 +1,12 @@
 package Assembler;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -24,6 +28,24 @@ public class Assembler {
 		List<File> list = new ArrayList<File>();
 		for (int i = 0; i <= numparts; ++i){
 			list.add(new File(filename+".part"+i));
+		}
+		
+		ArrayList<Long> checksums = new ArrayList<Long>();
+		try {
+			FileReader fr = new FileReader(filename + ".checksums.csv");
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			
+			while ((line = br.readLine()) != null){
+				checksums.add(Long.valueOf(line.substring(line.indexOf(',')+1, line.length())).longValue());
+			}
+			
+			br.close();
+			fr.close();
+		} catch (FileNotFoundException e){
+			e.printStackTrace();
+		} catch (IOException e){
+			e.printStackTrace();
 		}
 
 		System.out.println("Combining files: "+filename+".part0 to "+filename+".part"+numparts);
@@ -50,6 +72,11 @@ public class Assembler {
 			System.out.println("Time elapsed: " + diff.toMillis() + " ms");
 			Checksum checksum = new Checksum(ofile);
 			System.out.println(newfile + " checksum: " + checksum.getCheckSum());
+			
+			if (checksum.getCheckSum() == checksums.get(0)){
+				System.out.println("Assembled checksum matches checksum before being split");
+			}
+			
 			fos.close();
 			fos = null;
 		}catch (Exception e){
