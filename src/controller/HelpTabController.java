093ca50e6824b0c131dc3bc2f0bfc0195e4dc0c0
Unknown <dragon.tran@ou.edu>
2018-02-25 18:38:47 -0600

    Adds assemble view

--- src/controller/HelpTabController.java
+++ src/controller/HelpTabController.java
@@ -1,5 +1,27 @@
+/**
+ * Sample Skeleton for 'HelpTabView.fxml' Controller Class
+ */
+
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+
 public class HelpTabController {
+
+    @FXML // fx:id="helpBtn"
+    private Button helpBtn; // Value injected by FXMLLoader
+
+    @FXML
+    void helpMePlease(ActionEvent event) {
+    	Alert alert = new Alert(AlertType.INFORMATION);
+    	alert.setTitle("please help");
+    	alert.setHeaderText(null);
+    	alert.setContentText("help");
+    	alert.showAndWait();
+    }
 
 }
