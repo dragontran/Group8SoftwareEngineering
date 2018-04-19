83b2f59bb28e7df038335582f544acc78a062835
Addison <addison.womack@ou.edu>
2018-04-19 14:19:25 -0500

    Creation of help tab

--- src/main/java/com/caci/controller/HelpTabController.java
+++ src/main/java/com/caci/controller/HelpTabController.java
@@ -1,27 +1,63 @@
-/**
- * Sample Skeleton for 'HelpTabView.fxml' Controller Class
- */
-
 package main.java.com.caci.controller;
 
+import java.io.File;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
+import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 
 public class HelpTabController {
 
-    @FXML // fx:id="helpBtn"
-    private Button helpBtn; // Value injected by FXMLLoader
+    private String imageDirectory = "/src/main/java/com/caci/images/";
 
     @FXML
-    void helpMePlease(ActionEvent event) {
-    	Alert alert = new Alert(AlertType.INFORMATION);
-    	alert.setTitle("please help");
-    	alert.setHeaderText(null);
-    	alert.setContentText("help");
-    	alert.showAndWait();
+    private TextArea titleArea;
+
+    @FXML
+    private TextArea step1Area;
+
+    @FXML
+    private TextArea step2Area;
+
+    @FXML
+    private TextArea step3Area;
+
+    @FXML
+    private TextArea step4Area;
+
+    @FXML
+    private ImageView imageView;
+
+
+    @FXML
+    void displayStep1Image(MouseEvent event) {
+        loadImage(imageDirectory + "sourceDirectory.jpg");
     }
 
+    @FXML
+    void displayStep2Image(MouseEvent event) {
+        loadImage(imageDirectory + "outputDirectory.jpg");
+    }
+
+    @FXML
+    void displayStep3Image(MouseEvent event) {
+        loadImage(imageDirectory + "fileParts.jpg");
+    }
+
+    @FXML
+    private void loadImage(String filePath) {
+        Image image = new Image(filePath);
+        imageView.setImage(image);
+    }
 }
